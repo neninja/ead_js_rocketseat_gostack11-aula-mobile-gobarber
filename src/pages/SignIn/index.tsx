1bb928133a2c548da7e3f90b264521faee5122a6
Felipe Silva <felipe.dacs@gmail.com>
2020-05-23 18:48:38 -0300

    release: add aula 7

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import { Image } from 'react-native';
 
-import { Container, Title } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 // O react-native vai escolher se vai usar logo, logo@2x ou logo@3x
 import logoPng from '../../assets/logo.png';
+
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoPng} />
+
       <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button
+        onPress={() => {
+          console.log('debug');
+        }}
+      >
+        Entrar
+      </Button>
     </Container>
   );
 };
