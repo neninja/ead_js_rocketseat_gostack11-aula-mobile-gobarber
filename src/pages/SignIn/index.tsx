03dc2dc3fedd604210c338b8a154a3a2af96f1cd
Felipe Silva <felipe.dacs@gmail.com>
2020-05-23 17:07:16 -0300

    release: add aula 5

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Image } from 'react-native';
 
 import { Container } from './styles';
 
+// O react-native vai escolher se vai usar logo, logo@2x ou logo@3x
+import logoPng from '../../assets/logo.png';
+
 const SignIn: React.FC = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Image source={logoPng} />
+    </Container>
+  );
 };
 
 export default SignIn;
